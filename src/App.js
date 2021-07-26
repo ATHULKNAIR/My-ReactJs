01b1b51d925f36155999af91663a2c9073352503
ATHULKNAIR <athul2639@gmail.com>
2021-07-26 23:17:02 +0530

    Components included

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ import ReactNotifications from './components/ReactNotifications';
 import ReactModal from './components/ReactModal';
 import ReactTooltip from './components/ReactTooltip';
 import ReactCountUp from './components/ReactCountUp';
+import ReactIdleTimer from './components/ReactIdleTimer';
+import ReactColorPicker from './components/ReactColorPicker';
+import ReactCreditCards from './components/ReactCreditCards';
+import ReactDatePicker from './components/ReactDatePicker';
+import ReactVideoPlayer from './components/ReactVideoPlayer';
+import ReactSpinners from './components/ReactSpinners';
+import ReactChart from './components/ReactChart';
+
 
 Modal.setAppElement('#root')
     //   Because of accessibility reasons, we need to define the app element on our modal import 
@@ -16,21 +24,22 @@ Modal.setAppElement('#root')
 
 function App() {
   return (
-
-    
       <div className="App">
         <ReactIcons />
+        <ReactVideoPlayer/>
+        <ReactDatePicker/>
+        <ReactCreditCards/>
         <ReactNotifications/>
+        <ReactSpinners/>
         <ReactModal/>
         <ReactTooltip/>
         <ReactCountUp/>
+        <ReactIdleTimer/>
+        <ReactColorPicker/>
+        <ReactChart/>
       </div>
-   
 
-
-    
-
-  );
+);
 }
 
 export default App;
