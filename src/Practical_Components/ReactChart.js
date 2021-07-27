8b2be0ba23842badcf9a512f4a87bd74402e3968
ATHULKNAIR <athul2639@gmail.com>
2021-07-27 10:51:32 +0530

    React Chart Usage

---
+++ src/Practical_Components/ReactChart.js
@@ -0,0 +1,29 @@
+// npm install react-chartjs-2 chart.js
+
+import React from 'react'
+import {Line,Bar,Doughnut} from 'react-chartjs-2'
+
+function ReactChart() {
+    const data ={
+        labels:['Jan','Feb','Mar','Apr','May'],
+        datasets:[{
+            label:'Sales for 2021 (M)',
+            data:[3,2,2,1,5],
+           
+        },{
+            label:'Sales for 2020 (M)',
+            data:[1,3,2,2,3]
+        }]
+    }
+    
+    return  (
+        <div style={{height:'200px',width:'500px', marginLeft:'500px'}}>
+            <Line data={data}  />
+            <Bar data ={data} />
+            <Doughnut data ={data} />
+        </div>
+    )
+
+}
+
+export default ReactChart
